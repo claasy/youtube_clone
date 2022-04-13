ceea0c999777efc5a07b05cf13eab4796b4b070b
Amanda <aclaas522@gmail.com>
2022-04-13 13:22:35 -0500

    Worked on Functionality in LocalSetting File

--- backend/drf_jwt_backend/local_settings.py
+++ backend/drf_jwt_backend/local_settings.py
@@ -1,16 +1,22 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ';asfdgijfadsjknasdfkljafsdkjlfadkjlf'
+SECRET_KEY = 'django-insecure--;asfdgijfadsjknasdfkljafsdkjlfadkjlf'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'youtube_database',
-        'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'password'
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+        
+        
     }
 }
 
