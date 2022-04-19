0e468cfadc2b7d0d3a8ad382313df7f3f290e243
Justin Goncalves <jvgoncalves85@gmail.com>
2022-04-19 11:04:37 -0400

    Created info for App.js and modded HomePage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // General Imports
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
@@ -16,6 +17,37 @@ import Footer from "./components/Footer/Footer";
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
+  
+
+      const [videos, setVideos] = useState([{}])
+      useEffect(() => {
+        getAllComments();
+      }, [])
+      async function getAllComments(){
+        let response = await axios.get('http://127.0.0.1:8000/api/comments/');
+        setVideos(response.data);
+      }
+      async function addNewComment(entry){
+        let response = await axios.post('http://127.0.0.1:8000/api/comments/', entry);
+        if(response.status === 201){
+          await getAllComments();
+        }
+        let tempEntries = [...videos, entry];
+        setVideos(tempEntries);
+      }
+      async function deleteVideo(id){
+        let response = await axios.delete('http://127.0.0.1:8000/api/comments/', [id])
+        if(response.status === 201){
+          await getAllComments();
+      }
+  
+      
+
+    
+  }
+
+
+
   return (
     <div>
       <Navbar />
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -8,30 +8,30 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchComments = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/comments/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setComments(response.data);
       } catch (error) {
         console.log(error.message);
       }
     };
-    fetchCars();
+    fetchComments();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      {comments &&
+        comments.map((comment) => (
+          <p key={comment.id}>
+            {comment.year} {comment.model} {comment.make}
           </p>
         ))}
     </div>
